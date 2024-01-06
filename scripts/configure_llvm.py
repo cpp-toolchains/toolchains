40588bac4f0030a7c8f315eb516f5d088041fef1
Alexander Batashev <alexbatashev@outlook.com>
2024-01-06 20:06:16 +0300

    add test config

---
+++ scripts/configure_llvm.py
@@ -0,0 +1,117 @@
+import argparse
+import platform
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--build_dir", type=str, required=True)
+
+args = parser.parse_args()
+
+llvm_tools = [
+    "dsymutil",
+    "llvm-ar",
+    "llvm-cxxfilt",
+    "llvm-cov",
+    "llvm-dwarfdump",
+    "llvm-nm",
+    "llvm-objdump",
+    "llvm-objcopy",
+    "llvm-profdata",
+    "llvm-ranlib",
+    "llvm-readobj",
+    "llvm-strip",
+    "llvm-size",
+    "llvm-symbolizer"
+]
+
+runtime_targets = []
+targets_to_build = []
+
+if platform.system() == "Linux":
+    runtime_targets = [
+        "x86_64-unknown-linux-gnu",
+        "aarch64-unknown-linux-gnu",
+        "riscv64-unknown-linux-gnu",
+        "armv7-unknown-linux-gnueabihf",
+    ]
+    targets_to_build = [
+        "X86",
+        "AArch64",
+        "ARM",
+        "NVPTX",
+        "AMDGPU",
+        "RISCV",
+        "WebAssembly",
+        "BPF",
+    ]
+elif platform.system() == "Darwin":
+    if platform.processor() == "arm":
+        runtime_targets = ["aarch64-apple-darwin"]
+    else:
+        runtime_targets = ["x86_64-apple-darwind"]
+    targets_to_build = ["X86", "AArch64", "WebAssembly"]
+
+cmake_args = [
+    "cmake",
+    "-GNinja",
+    "-B",
+    args.build_dir,
+    "-S",
+    "llvm/llvm",
+    "-DCMAKE_BUILD_TYPE=Release",
+    "-DCMAKE_INSTALL_PREFIX=/usr/local",
+    "-DLLVM_TARGETS_TO_BUILD={}".format(';'.join(targets_to_build)),
+    "-DLLVM_RUNTIME_TARGETS={}".format(';'.join(runtime_targets)),
+    "-DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON",
+    "-DLLVM_ENABLE_TERMINFO=OFF",
+    "-DLLVM_ENABLE_ZLIB=OFF",
+    "-DLLVM_ENABLE_ZSTD=OFF",
+    "-DCLANG_DEFAULT_CXX_STDLIB=libc++",
+    "-DCLANG_DEFAULT_RTLIB=compiler-rt",
+    "-DCLANG_DEFAULT_UNWINDLIB=libunwind",
+    "-DCLANG_DEFAULT_LINKER=lld",
+    "-DLLVM_ENABLE_RUNTIMES=libunwind;compiler-rt;libcxx;libcxxabi;openmp",
+    "-DLLVM_ENABLE_PROJECTS=bolt;clang;clang-tools-extra;lld;pstl",
+    "-DLLVM_STATIC_LINK_CXX_STDLIB=ON",
+    "-DLLVM_INSTALL_TOOLCHAIN_ONLY=ON",
+    "-DLLVM_TOOLCHAIN_TOOLS={}".format(';'.join(llvm_tools)),
+    "-DLLVM_DISTRIBUTIONS=ClangTidy;ClangDoc;ClangFormat;ClangdTool;BoltTool;LldTool;StdLib;Toolchain",
+    "-DLLVM_ClangTidy_DISTRIBUTION_COMPONENTS=clang-tidy",
+    "-DLLVM_ClangDoc_DISTRIBUTION_COMPONENTS=clang-doc",
+    "-DLLVM_ClangFormat_DISTRIBUTION_COMPONENTS=clang-format",
+    "-DLLVM_ClangdTool_DISTRIBUTION_COMPONENTS=clangd",
+    "-DLLVM_BoltTool_DISTRIBUTION_COMPONENTS=bolt",
+    "-DLLVM_LldTool_DISTRIBUTION_COMPONENTS=lld",
+    "-DLLVM_StdLib_DISTRIBUTION_COMPONENTS=runtimes",
+    "-DLLVM_Toolchain_DISTRIBUTION_COMPONENTS=clang;clang-format;clang-tidy;clang-resource-headers;bolt;runtimes;lld;{}".format(';'.join(llvm_tools)),
+]
+
+if platform.system() == "Darwin":
+    cmake_args.append("-DRUNTIMES_BUILD_ALLOW_DARWIN=ON")
+
+for rt in runtime_targets:
+    cmake_args.extend([
+        f"-DRUNTIMES_{rt}_OPENMP_LIBDIR_SUFFIX={rt}",
+        f"-DRUNTIMES_{rt}_OPENMP_STANDALONE_BUILD=ON",
+        f"-DRUNTIMES_{rt}_OPENMP_LLVM_TOOLS_DIR={args.build_dir}/bin",
+        f"-DRUNTIMES_{rt}_LIBCXX_HERMETIC_STATIC_LIBRARY=ON",
+        f"-DRUNTIMES_{rt}_LIBCXXABI_USE_LLVM_UNWINDER=ON",
+        f"-DRUNTIMES_{rt}_LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON",
+        f"-DRUNTIMES_{rt}_LIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=ON",
+    ])
+
+    if rt != "x86_64-unknown-linux-gnu":
+        cmake_args.extend([
+            f"-DRUNTIMES_{rt}_OPENMP_ENABLE_LIBOMPTARGET=OFF",
+            f"-DRUNTIMES_{rt}_LIBOMP_OMPD_GDB_SUPPORT=OFF",
+        ])
+
+if platform.system() == "Linux":
+    cmake_args.extend([
+        "-DCMAKE_C_COMPILER=clang",
+        "-DCMAKE_CXX_COMPILER=clang++",
+        "-DLLVM_USE_LINKER=mold"
+    ])
+
+print(' '.join(cmake_args))
+subprocess.run(cmake_args, check=True, stdout=subprocess.PIPE)
---
+++ scripts/update_llvm_configs.py
@@ -0,0 +1,46 @@
+import argparse
+import time
+import hashlib
+import os
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tag", type=str, required=True)
+
+args = parser.parse_args()
+
+all_os = ["linux", "windows", "macos"]
+all_cpu = ["aarch64", "riscv64", "x86_64"]
+all_artifacts = ["clang", "clang-tools", "clangd", "bolt", "libcxx", "lld"]
+
+today = time.strftime("%Y-%m-%d")
+
+readme = ""
+
+for os_ in all_os:
+  for cpu in all_cpu:
+    for art in all_artifacts:
+      try:
+        path = f"artifacts/{args.tag}-{os_}-{cpu}-{art}.tar.zst"
+        f = open(path, 'rb')
+        hash = hashlib.file_digest(f, 'sha256').hexdigest()
+        f.close()
+
+        config_path = f"llvm/{os_}/{cpu}/{art}.json"
+        config_file = open(config_path, 'r')
+        data = json.load(config_file)
+        config_file.close()
+        url = f"https://github.com/cpp-toolchains/toolchains/releases/download/{args.tag}-{today}/{args.tag}-{os_}-{cpu}-{art}.tar.zst"
+        data[args.tag]["url"] = url
+        data[args.tag]["sha256"] = hash
+        config_file = open(config_file, 'w')
+        config_file.write(json.dumps(data, indent=4))
+        config_file.close()
+
+        readme += f"{hash} {args.tag}-{os_}-{cpu}-{art}.tar.zst\n"
+      except:
+        pass
+
+f = open("RELEASE_NOTES.txt", 'w')
+f.write(readme)
+f.close()
